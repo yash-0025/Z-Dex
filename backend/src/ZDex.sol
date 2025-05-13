5a764277ecf21cd12db8ecf53f659d985b847b2e
yash-0025 <yashpatel200403@gmail.com>
2025-05-13 23:47:59 +0530

    # BACKEND :: SMART CONTRACT CREATED

--- backend/src/ZDex.sol
+++ backend/src/ZDex.sol
@@ -11,6 +11,8 @@ contract ZDex is ReentrancyGuard {
 
     event LiquidityAdded(address provider, uint256 ethAmount, uint256 tokenAmount);
     event LiquidityRemoved(address provider, uint256 ethAmount, uint256 tokenAmount);
+    event PurchaseToken(address buyer, uint256 ethAmount, uint256 tokenAmount);
+    event PurchaseETH(address seller, uint256 tokenAmount, uint256 ethAmount);
 
     constructor(address _token, address _priceFeed) {
         token = IERC20(_token);
@@ -55,3 +57,44 @@ function removeLiquidity(uint256 liquidityTokens) public nonReentrant {
 }
 
 
+function ethToTokenSwap(uint256 minTokens) public payable nonRenetrant {
+    require(msg.value > 0, "ETH amount should be greater than zero");
+    uint256 tokenAmount = getInputAmount(tokenAmount, tokenReserve, ethReserve);
+    require(ethAmount >= mintEth, "Insufficient output amount");
+
+    ethReserve += msg.value;
+    tokenReserve -= tokenAmount;
+
+    require(token.transfer(msg.sender, tokenAmount), "Failed to transfer token");
+    emit PurchaseToken(msg.sender, msg.value, tokenAmount);
+}
+
+function getInputAmount(uint256 inputAmount, uint256 inputReserve, uint256 outputReserve) internal pure returns(uint256){
+    require(inputReserve > 0 && outputReserve > 0, "Invalid reserve");
+    uint256 inputAmountWithFee = inputAmount * 997;
+    uint256 numerator = inputAmountWithFee * outputReserve;
+    uint256 denominator = (inputReserve *1000) + inputAmountWithFee;
+    return numeratore / denominator;
+}
+
+function tokenToEthSwap(uint256 tokenAmount, uin256 minEth) public nonReentrant {
+    require(tokenAmount > 0, "Token should be more than zero");
+    uint256 ethAmount = getInputAmount(tokenAmount, tokenReserve, ethReserve);
+    require(ethAmount >= minEth, "Insufficient amount to swap");
+
+    tokenReserve += tokenAmount;
+    ethReserve -= ethAmount;
+
+    require(token.transferFrom(msg.sender, address(this), tokenAmount), "Failed while transferring tokens");
+    payable(msg.sender).transfer(ethAmount);
+    emit PurchaseETH(msg.sender, tokenAmount, ethAmount);
+}
+
+
+function getTokenAmount(uint256 ethAmount) public view returns (uint256) {
+    return getInputAmount(ethAmount, ethReserve, tokenReserve);
+}
+
+function getEthAmount(uint256 tokenAmount) public view returns(uint256) {
+    return getInputAmount(tokenAmount, tokenReserve, ethReserve);
+}
